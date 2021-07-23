0f8f64bd86a81b8b194126756f7fc65aed2452e7
Erasmo J.F Da Silva <erasmogohiper@gmail.com>
2021-07-23 14:02:43 -0300

    CM - Let's set up the Core Data project with SwiftUI and fix Xcode's template
    Let's set up the Core Data project with SwiftUI
    and fix Xcode's template

---
+++ Devote/Devote/DevoteApp.swift
@@ -0,0 +1,22 @@
+//
+//  DevoteApp.swift
+//  Devote
+//
+//  Created by Erasmo J.F Da Silva on 23/07/21.
+//  SwiftUI ♡ Better Apps. Less Code
+//  https://erasmojf.github.io/
+//  Fidju de Bideira de Feira de Caracol
+
+import SwiftUI
+
+@main
+struct DevoteApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
