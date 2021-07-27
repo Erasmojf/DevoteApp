09a9b98be11cd53b8cfad0f5d580d253b6b38085
Erasmo J.F Da Silva <erasmogohiper@gmail.com>
2021-07-27 11:31:39 -0300

    CM - Let's develop a custom header
    Let's develop a custom header

--- Devote/Devote/DevoteApp.swift
+++ Devote/Devote/DevoteApp.swift
@@ -12,11 +12,12 @@ import SwiftUI
 @main
 struct DevoteApp: App {
     let persistenceController = PersistenceController.shared
-
+    @AppStorage("isDarkMode") private var isDarkMode: Bool = false
     var body: some Scene {
         WindowGroup {
             ContentView()
                 .environment(\.managedObjectContext, persistenceController.container.viewContext)
+                .preferredColorScheme(isDarkMode ? .dark : .light)
         }
     }
 }
--- Devote/Devote/View/NewTaskItemView.swift
+++ Devote/Devote/View/NewTaskItemView.swift
@@ -11,6 +11,7 @@ import SwiftUI
 
 struct NewTaskItemView: View {
     // MARK: - PROPERTIES
+    @AppStorage("isDarkMode") private  var isDarkMode: Bool = false
     @Environment(\.managedObjectContext) private var viewContext
     @State private var task: String = ""
     
@@ -52,7 +53,8 @@ struct NewTaskItemView: View {
                     .font(.system(size: 24, weight: .bold, design: .rounded))
                     .padding()
                     .background(
-                        Color(UIColor.systemGray6)
+                        isDarkMode ? Color(UIColor.tertiarySystemGroupedBackground) :
+                            Color(UIColor.secondarySystemBackground)
                     )
                     .cornerRadius(10)
                 
@@ -73,7 +75,7 @@ struct NewTaskItemView: View {
             .padding(.horizontal)
             .padding(.vertical, 16)
             .background(
-                Color.white
+                isDarkMode ? Color(UIColor.secondarySystemBackground) :  Color.white
             )
             .cornerRadius(16)
             .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.65), radius: 24)
